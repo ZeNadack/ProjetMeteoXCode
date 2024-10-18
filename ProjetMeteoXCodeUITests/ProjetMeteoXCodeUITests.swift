a0393f77fa0c62c746dd23214200da43e5ae1d7e
ZeNadack <t.blanchard52000@gmail.com>
2024-10-18 17:43:49 +0200

    Commit du TestUI

--- ProjetMeteoXCodeUITests/ProjetMeteoXCodeUITests.swift
+++ ProjetMeteoXCodeUITests/ProjetMeteoXCodeUITests.swift
@@ -7,35 +7,33 @@
 
 import XCTest
 
-final class ProjetMeteoXCodeUITests: XCTestCase {
+class ProjetMeteoXCodeUITests: XCTestCase {
+
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+        func testWeatherFetchingUI() throws {
+        let cityTextField = app.textFields["Entrez le nom d'une ville"]
+        XCTAssertTrue(cityTextField.exists)
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+        cityTextField.tap()
+        cityTextField.typeText("Paris")
+        
+        let getWeatherButton = app.buttons["Obtenir la météo"]
+        XCTAssertTrue(getWeatherButton.exists)
+        
+        getWeatherButton.tap()
+        
+        
         }
-    }
 }
