8893acdb0b62ca9ade4b310a0fea259b4c98c77b
ZeNadack <t.blanchard52000@gmail.com>
2024-10-18 09:01:35 +0200

    Initial Commit

---
+++ ProjetMeteoXCode/ProjetMeteoXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetMeteoXCodeApp.swift
+//  ProjetMeteoXCode
+//
+//  Created by Theo Blanchard on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjetMeteoXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
